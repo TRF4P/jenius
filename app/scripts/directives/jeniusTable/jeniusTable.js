5a6bada4bc2235df7a89d01ba760d1bb3e93adab
clarknoah <noahbc08@gmail.com>
2014-06-04 08:03:42 -0400

    dfdf
    df

---
+++ app/scripts/directives/jeniusTable/jeniusTable.js
@@ -0,0 +1,12 @@
+'use strict';
+angular.module('jeniusApp.directives.jeniusTable')
+    .directive('jeniusTable', function(CommonServices) {
+        return {
+            templateUrl: '/scripts/directives/jeniusTable/jeniusTable.html',
+            //restrict: 'A',
+            scope: {},
+            link: function(scope, element, attr) {
+
+            }
+        };
+    })
